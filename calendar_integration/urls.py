c2572e6b34938b4d09100a4ae0faa06f6be49b67
arshadaqeel11566@gmail.com <arshadaqeel11566@gmail.com>
2023-05-29 14:45:02 +0530

    Initial Commit

---
+++ calendar_integration/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework import routers
+from calendar_api.views import GoogleCalendarInitView, GoogleCalendarRedirectView
+
+router = routers.DefaultRouter()
+
+urlpatterns = router.urls
+
+urlpatterns += [
+    path('rest/v1/calendar/init/', GoogleCalendarInitView.as_view(), name='calendar-init'),
+    path('rest/v1/calendar/redirect/', GoogleCalendarRedirectView.as_view(), name='calendar-redirect'),
+]
